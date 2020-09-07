4a328ba48e70dcdce0e8243f6a8d2efd2137bded
jjinaH <wlsdk2769@ajou.ac.kr>
2020-09-07 14:18:18 +0900

    Modify repository class

--- src/main/java/com/o2o/action/server/repository/JDBCTest.java
+++ src/main/java/com/o2o/action/server/repository/JDBCTest.java
@@ -3,34 +3,27 @@ package com.o2o.action.server.repository;
 import java.sql.*;
 
 public class JDBCTest {
-    public static void main(String[] args) {
-//
-//        Connection conn = null;
-//        Statement st = null;
-//        ResultSet rs = null;
-//
-//
-//        String url = "jdbc:postgresql://localhost";
-//        String user = "postgres";
-//        String password = "jahwang";
-//
-//        try {
-//            conn = DriverManager.getConnection(url, user, password);
-//            st = conn.createStatement();
-//            rs = st.executeQuery("SELECT VERSION()");
-//
-//            if (rs.next())
-//                System.out.println(rs.getString(1));
-//        } catch (SQLException sqlEX) {
-//            System.out.println(sqlEX);
-//        } finally {
-//            try {
-//                rs.close();
-//                st.close();
-//                conn.close();
-//            } catch (SQLException sqlEX) {
-//                System.out.println(sqlEX);
-//            }
-//        }
+
+    private Connection conn;
+
+    public JDBCTest() {
+
+        try {
+
+            Statement st = null;
+            ResultSet rs = null;
+
+            String url = "jdbc:postgresql://localhost:5432/";
+            String user = "postgres";
+            String password = "jahwang";
+
+            conn = DriverManager.getConnection(url, user, password);
+            st = conn.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+    
+
 }
